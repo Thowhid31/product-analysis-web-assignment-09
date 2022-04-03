ea21d4099d55e8f6fd67ae7cf764fd976322d0e6
Thowhid31 <thowhidantor16@gmail.com>
2022-04-03 23:02:28 +0600

    added the review section

--- src/Components/Book/Book.js
+++ src/Components/Book/Book.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import './Book.css'
 
 const Book = (props) => {
-    const {name, ratings, img, price, reviewer} = props.book;
+    const {name, ratings, img, price, reviewer, review} = props.book;
     return (
-        <div>
+        <div className='cart-container'>
             <div>
                 <img src={img} alt="" />
             </div>
-            <div>
+            <div className='review-container'>
                 <h1>Reviewer: {reviewer}</h1>
                 <h3>Name: {name}</h3>
                 <p>Price: {price}</p>
                 <p>Ratings: {ratings}</p>
+                <h3>Review: {review}</h3>
             </div>
         </div>
     );
--- src/Components/Review/Review.js
+++ src/Components/Review/Review.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Book from '../Book/Book';
-import useBooks from '../Books/useBooks';
+import useBooks from '../hooks/useBooks';
 
 const Review = () => {
     const [books, setBooks] = useBooks()
     return (
         <div>
             {
-                books.map(book => <Book></Book>)
+                books.map(book => <Book
+                key={book.id}
+                book={book}></Book>)
             }
         </div>
     );
