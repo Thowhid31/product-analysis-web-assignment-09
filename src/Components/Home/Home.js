ed9005e64808626019c668e6ba8648f08bf8ad64
Thowhid31 <thowhidantor16@gmail.com>
2022-04-04 14:36:50 +0600

    added the section of home button see more

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Review from '../Review/Review';
 import './Home.css'
 
 const Home = () => {
     return (
-        <div className='home-container'>
-            <div className='h1-container-home'>
-                <h1>Explore Our <br></br> Wimpy Kid<br></br>Collection...</h1>
+        <div>
+            <div className='home-container'>
+                <div className='h1-container-home'>
+                    <h1>Explore Our <br></br> Wimpy Kid<br></br>Collection...</h1>
+                </div>
+                <div className='home-img'>
+                    <img src="https://static.wikia.nocookie.net/doawk/images/d/d8/9781419729454-350x522.jpg" alt="" />
+                </div>
             </div>
-            <div className='home-img'>
-                <img src="https://static.wikia.nocookie.net/doawk/images/d/d8/9781419729454-350x522.jpg" alt="" />
+            <div>
+                <h2>Here some reviews of our Customers</h2>
+                
+            </div>
+            <div>
+                <button className='review-btn'>
+                    <Link to='/review'>See More</Link>
+                </button>
             </div>
         </div>
+
     );
 };
 
