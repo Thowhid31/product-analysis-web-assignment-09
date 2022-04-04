070b704757ce35b532d2bcf132397901039fbd30
Thowhid31 <thowhidantor16@gmail.com>
2022-04-04 14:24:47 +0600

    added the section of home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            <h1>Hello Home</h1>
+        <div className='home-container'>
+            <div className='h1-container-home'>
+                <h1>Explore Our <br></br> Wimpy Kid<br></br>Collection...</h1>
+            </div>
+            <div className='home-img'>
+                <img src="https://static.wikia.nocookie.net/doawk/images/d/d8/9781419729454-350x522.jpg" alt="" />
+            </div>
         </div>
     );
 };
